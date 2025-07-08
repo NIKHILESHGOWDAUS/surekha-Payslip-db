80a3b3ceefdb1b17f6163d3dd519c36afae922fb
Nikhilesh <nikhileshgowda10@gmail.com>
2025-07-08 15:58:32 -0700

    commit2

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3500;
+const port = 3609;
 
 // PostgreSQL database connection
 const pool = new Pool({
@@ -114,5 +114,5 @@ app.get('/api/payslips/:empCode', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://100.29.186.245:${port}`);
+    console.log(`Server running on http://3.85.61.23:${port}`);
 });
